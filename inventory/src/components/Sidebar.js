e84f13f5c573f2e27b3865e601dbf1bebaeb663c
mani kitgwim christopher <kitgwimmani@gmail.com>
2024-01-25 01:30:15 +0100

    sidebar editted

--- inventory/src/components/Sidebar.js
+++ inventory/src/components/Sidebar.js
@@ -15,7 +15,7 @@ const Sidebar = () => {
       <CDBSidebar textColor="#fff" backgroundColor="#355">
         <CDBSidebarHeader prefix={<i className="fa fa-bars fa-large"></i>}>
           <a href="/" className="text-decoration-none" style={{ color: 'inherit' }}>
-            Sidebar
+            GHAA Inventory
           </a>
         </CDBSidebarHeader>
 
@@ -24,18 +24,30 @@ const Sidebar = () => {
             <NavLink exact to="/" activeClassName="activeClicked">
               <CDBSidebarMenuItem icon="columns">Dashboard</CDBSidebarMenuItem>
             </NavLink>
-            <NavLink exact to="/tables" activeClassName="activeClicked">
-              <CDBSidebarMenuItem icon="table">Tables</CDBSidebarMenuItem>
+            <NavLink exact to="/" activeClassName="activeClicked">
+              <CDBSidebarMenuItem icon="user">Manage Users</CDBSidebarMenuItem>
             </NavLink>
-            <NavLink exact to="/profile" activeClassName="activeClicked">
-              <CDBSidebarMenuItem icon="user">Profile page</CDBSidebarMenuItem>
+            <NavLink exact to="/location" activeClassName="activeClicked">
+              <CDBSidebarMenuItem icon="city">Locations</CDBSidebarMenuItem>
             </NavLink>
-            <NavLink exact to="/analytics" activeClassName="activeClicked">
-              <CDBSidebarMenuItem icon="chart-line">Analytics</CDBSidebarMenuItem>
+            <NavLink exact to="/unit" activeClassName="activeClicked">
+              <CDBSidebarMenuItem icon="chart-line">Inventory Unit</CDBSidebarMenuItem>
             </NavLink>
 
-            <NavLink exact to="/hero404" target="_blank" activeClassName="activeClicked">
-              <CDBSidebarMenuItem icon="exclamation-circle">404 page</CDBSidebarMenuItem>
+            <NavLink exact to="/custodian"  activeClassName="activeClicked">
+              <CDBSidebarMenuItem icon="user">Custodian</CDBSidebarMenuItem>
+            </NavLink>
+
+            <NavLink exact to="/supplier"  activeClassName="activeClicked">
+              <CDBSidebarMenuItem icon="car">Supplier</CDBSidebarMenuItem>
+            </NavLink>
+
+            <NavLink exact to="/receiver"  activeClassName="activeClicked">
+              <CDBSidebarMenuItem icon="user">Receiver</CDBSidebarMenuItem>
+            </NavLink>
+
+            <NavLink exact to="/supply"  activeClassName="activeClicked">
+              <CDBSidebarMenuItem icon="table">Supply</CDBSidebarMenuItem>
             </NavLink>
           </CDBSidebarMenu>
         </CDBSidebarContent>
@@ -46,7 +58,7 @@ const Sidebar = () => {
               padding: '20px 5px',
             }}
           >
-            Sidebar Footer
+           
           </div>
         </CDBSidebarFooter>
       </CDBSidebar>
